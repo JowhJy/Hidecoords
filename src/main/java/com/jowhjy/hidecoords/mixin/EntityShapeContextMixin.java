ecc316ad43948a7bd40c03cb8d7cbfc0f29d8773
JowhJy <johanwhoek@gmail.com>
2025-03-20 20:20:29 +0100

    removed collision with bamboo and dripstone for players

---
+++ src/main/java/com/jowhjy/hidecoords/mixin/EntityShapeContextMixin.java
@@ -0,0 +1,34 @@
+package com.jowhjy.hidecoords.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.EntityShapeContext;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(EntityShapeContext.class)
+public class EntityShapeContextMixin {
+    @Shadow
+    @Final
+    private Entity entity;
+
+    @WrapOperation(method = "getCollisionShape", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getCollisionShape(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/ShapeContext;)Lnet/minecraft/util/shape/VoxelShape;"))
+    public VoxelShape hidecoords$playerNoCollideWithBambooOrDripstone(BlockState instance, BlockView blockView, BlockPos blockPos, ShapeContext shapeContext, Operation<VoxelShape> original) {
+        return
+                (this.entity.isPlayer() && (instance.isOf(Blocks.BAMBOO_BLOCK) || instance.isOf(Blocks.POINTED_DRIPSTONE)))
+                        ? VoxelShapes.empty()
+                        : original.call(instance, blockView, blockPos, shapeContext);
+
+    }
+}
